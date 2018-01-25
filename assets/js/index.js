4b38f4db9acf40030fc8c13bd3be6609cda75ebe
Sam Landfried <slandfried@bignerdranch.com>
2018-01-25 16:04:53 -0700

    Add appendTodo and implement Add Todo form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form')
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = event.target.querySelector('input[type="text"]');
+    const todo = input.value;
+
+    appendTodo(todo);
+    input.value = '';
+  }); 
+});
+
+const appendTodo = todo => {
+  const list = document.querySelector('ol');
+  const listItems = list.querySelectorAll('li');
+  const lastId = listItems.item(list.childElementCount - 1).id;
+  const li = document.createElement('li')
+
+  li.innerText = todo;
+  li.id = parseInt(lastId) + 1;
+  li.className = 'list-group-item';
+  list.appendChild(li);
+};
+
 const destroyTodo = id => {
   const list = document.querySelector('ol');
   const todo = list.querySelector(`[id='${id}']`);
