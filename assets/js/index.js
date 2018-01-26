0911baaa67744f24cd91f0e1c84291a50e514752
Sam Landfried <slandfried@bignerdranch.com>
2018-01-25 20:10:53 -0700

    Add changes in screencast

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,7 @@ const appendTodo = todo => {
   li.innerText = todo;
   li.id = parseInt(lastId) + 1;
   li.className = 'list-group-item';
+  li.setAttribute('draggable', true);
   list.appendChild(li);
 };
 
@@ -17,8 +18,37 @@ const destroyTodo = id => {
   return list.removeChild(todo); 
 };
 
+const dragStartHandler = event => {
+  const id = event.target.id;
+
+  event.dataTransfer.setData('text/plain', id);
+  event.dataTransfer.effectAllowed = 'move';
+};
+
+const dropHandler = event => {
+  const id = event.dataTransfer.getData('text');
+
+  event.target.classList.remove('shake');
+  destroyTodo(id);
+};
+
+const dragEnterHandler = event => {
+  event.target.classList.add('shake');
+};
+
+const dragLeaveHandler = event => {
+  event.target.classList.remove('shake');
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form')
+  ['Herd cats', 'Juggle manatees', 'Wrestle horses'].forEach(appendTodo);
+
+  const form = document.querySelector('form');
+  const trash = document.querySelector('img');
+
+  trash.addEventListener('drop', dropHandler);
+  trash.addEventListener('dragenter', dragEnterHandler);
+  trash.addEventListener('dragleave', dragLeaveHandler);
 
   form.addEventListener('submit', event => {
     event.preventDefault();
@@ -29,3 +59,5 @@ document.addEventListener('DOMContentLoaded', () => {
     input.value = '';
   }); 
 });
+
+document.addEventListener('dragstart', dragStartHandler);
